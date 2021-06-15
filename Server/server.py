bff423e11123f5f9eadc67090b92864c1b3fdd2e
ArmyaAli <ali.umar.work@gmail.com>
2021-06-14 22:58:34 -0400

    SQLLite3 db, basic connection, validating login

--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,24 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return "Log in successful!"
+        user = request.form["userbox"]
+        psw = request.form["passbox"]
+        # query the database
+        con = sqlite3.connect('database/test.db')
+        for row in con.cursor().execute('SELECT * FROM user'):
+            if row[1] == user and row[2] == psw:
+                return "Successful!"
+        return "Unsuccessful"
+
+@app.route("/water", methods=['POST'])
+def water():
+    pass
+
+@app.route("/schedule", methods=['POST'])
+def schedule():
+    pass
