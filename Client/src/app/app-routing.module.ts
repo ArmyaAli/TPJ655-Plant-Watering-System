4dece8e365735d89b4b233fd87b16e8cd06a1dee
ArmyaAli <ali.umar.work@gmail.com>
2021-07-11 10:39:44 -0400

    added template for dashboard page for after the login

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 
 const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: 'login', component: LoginPageComponent },
+    { path: 'dashboard', component: DashboardComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full'},
     { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
