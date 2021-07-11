cd8fa5c80603c5a76d049d1e2295f1fbb1f12e69
ArmyaAli <ali.umar.work@gmail.com>
2021-07-11 10:40:51 -0400

    handle login using a post req not form

--- Client/src/app/Pages/login-page/login-page.component.ts
+++ Client/src/app/Pages/login-page/login-page.component.ts
@@ -1,3 +1,7 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  url = '';
+  loginEndpoint = '';
+  user = '';
+  pass = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin($e: Event) {
+    this.http.post<any>(this.url, { username: this.user, password: this.pass});
+  }
+
+  updateUser(e: any) {
+    this.user = e.target.value;
+    console.log(this.user)
+  }
+
+  updatePass(e: any) {
+    this.pass = e.target.value;
+    console.log(this.pass)
   }
 
 }
