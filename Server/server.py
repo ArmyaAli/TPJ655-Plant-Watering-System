4e105af80455d7f07d292b719da2224537e4ddf3
ArmyaAli <ali.umar.work@gmail.com>
2021-07-15 18:34:30 -0400

    /login endpoint hookedup

--- Server/server.py
+++ Server/server.py
@@ -1,24 +1,40 @@
 from flask import Flask, request
 import sqlite3
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/login", methods=['GET', 'POST'])
+deviceState = {
+    "state": "idle",
+}
+
+# SERVER
+@app.route("/login", methods=['POST'])
 def login():
     if request.method == 'POST':
-        user = request.form["userbox"]
-        psw = request.form["passbox"]
-        # query the database
-        con = sqlite3.connect('database/test.db')
-        for row in con.cursor().execute('SELECT * FROM user'):
-            if row[1] == user and row[2] == psw:
-                return "Successful!"
-        return "Unsuccessful"
+        try:
+            user = request.json['username']
+            psw = request.json['password']
+            # query the database
+            con = sqlite3.connect('database/test.db')
+            for row in con.cursor().execute('SELECT * FROM user'):
+                if row[1] == str(user) and row[2] == str(psw):
+                    return { "status": "success" }
+            return { "status": "failiure" }
+        except:
+            return { "status": "server error"}
 
 @app.route("/water", methods=['POST'])
 def water():
+    deviceState["state"] = "watering"
     pass
 
+@app.route("/status", methods=['GET'])
+def status():
+    return {"status": "bruh"}
+    
 @app.route("/schedule", methods=['POST'])
 def schedule():
     pass
