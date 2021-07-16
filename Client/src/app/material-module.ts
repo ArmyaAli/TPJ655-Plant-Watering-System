6bef216499c08e1b3e05b9611d6168668f698d46
ArmyaAli <ali.umar.work@gmail.com>
2021-07-16 15:19:34 -0400

    Angular Material Date Calender used

--- Client/src/app/material-module.ts
+++ Client/src/app/material-module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
 
 /** ALL THE MODULES FROM ANGULAR MATERIAL SHOULD BE DECLARED HERE 
  *  TO NOT CLUTTER app.module.ts
  */
 @NgModule({
     exports: [
-        MatDialogModule
+        MatDialogModule,
+        MatDatepickerModule,
+        MatCardModule,
+        MatNativeDateModule 
+        
     ]
 })
 export class MaterialModule { }
