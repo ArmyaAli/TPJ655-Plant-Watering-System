5bddd0a0bbac4cb1f57fef60458a2e16c1333bc0
ArmyaAli <ali.umar.work@gmail.com>
2021-07-19 18:39:53 -0400

    restructure dashboard/add some styling

--- Client/src/app/Pages/dashboard/dashboard.component.ts
+++ Client/src/app/Pages/dashboard/dashboard.component.ts
@@ -10,21 +10,15 @@ import { globalStateService } from 'src/app/State/global';
   templateUrl: './dashboard.component.html',
   styleUrls: [
     './dashboard-styles/dashboard.component.css',
-    './dashboard-styles/dashboard-schedule.component.css',
     './dashboard-styles/dashboard-manual.component.css',
-    './dashboard-styles/dashboard-account.component.css',
-    './dashboard-styles/dashboard-device.component.css',
   ]
 })
 
 
 export class DashboardComponent implements OnInit {
-  url: string = '';
-  submitEndpoint = ''
-  triggerEndpoint = ''
   username = '';
 
-  constructor(private http: HttpClient, public dialog: MatDialog, private state: globalStateService) {
+  constructor(private http: HttpClient, private state: globalStateService) {
     this.username = this.state.username;
   }
 
@@ -32,12 +26,23 @@ export class DashboardComponent implements OnInit {
   }
 
   waterThePlant() {
-    console.log("I've been clicked");
-    this.http.post<any>(this.url, { data: 'Hello From the other side' });
+    const url = "http://166.48.21.182:1600/water";
+    this.http.post<string>(url, { status: "watering" }, { responseType: 'text' as 'json' }).subscribe(
+      (rawdata: any) => {
+        try {
+          const data = JSON.parse(rawdata);
+          
+        } catch (err) {
+          console.error('Error [Proc handleLogin]');
+        }
+      },
+      (error) => console.log(error)
+    );
   }
 
   submitSchedule() {
-    this.http.post<any>(this.url, { data: 'Hello From the other side' });
+    const url = "http://";
+    this.http.post<any>(url, { data: 'Hello From the other side' });
   }
 
   getAccountInfo() {
@@ -47,42 +52,5 @@ export class DashboardComponent implements OnInit {
   getDeviceStatus() {
 
   }
-
-  openDialog() {
-    const dialogRef = this.dialog.open(updateAccountDialog);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
 }
 
-@Component({
-  selector: 'app-dashboard-update-windows',
-  templateUrl: './update-windows/update-account.html',
-  styleUrls: [
-    './dashboard-styles/update-account.component.css',
-  ]
-})
-export class updateAccountDialog { 
-  constructor(private http: HttpClient) {
-
-  }
-
-  submitUpdatedInfo($e: any) {
-    try {
-      // this.http.post<any>(this.url, { data: 'Hello From the other side' });
-    } catch(err) {
-      console.error('Error [submitUpdatedInfo Proc]');
-    }
-  }
-}
-
-// @Component({
-//   selector: 'app-dashboard-update-schedule',
-//   templateUrl: './update-windows/update-schedule.html',
-//   styleUrls: [
-//     './dashboard-styles/update-schedule.component.css',
-//   ]
-// })
-// export class updateScheduleDialog { }
