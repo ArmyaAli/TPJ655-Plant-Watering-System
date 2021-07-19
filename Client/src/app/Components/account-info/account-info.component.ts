91aeec69372e2df53e3cbe0cc6fefba7af0269a8
ArmyaAli <ali.umar.work@gmail.com>
2021-07-19 18:37:35 -0400

    added fetching and displaying data from server

--- Client/src/app/Components/account-info/account-info.component.ts
+++ Client/src/app/Components/account-info/account-info.component.ts
@@ -1,17 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { globalStateService } from 'src/app/State/global';
+
+
+interface accountInfo {
+  username: string;
+  fName: string;
+  lName: string;
+  address: string;
+  model: string;
+};
 
 @Component({
   selector: 'app-account-info',
   templateUrl: './account-info.component.html',
   styleUrls: ['./account-info.component.css']
 })
-export class AccountInfoComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+export class AccountInfoComponent implements OnInit {
+  data: accountInfo | null;
+  constructor(public dialog: MatDialog, private http: HttpClient, private state: globalStateService, private route: ActivatedRoute) { 
+    this.data = null;
+    this.route.queryParams.subscribe(params => {
+        this.state.username = params['user'];
+    });
+  }
 
   ngOnInit(): void {
+    // we can make a server request
+    const accountInfoUrl = 'http://127.0.0.1:5000/accountInfo'
+    this.http.get<any>(accountInfoUrl, {
+      params: {
+        user: this.state.username
+      }
+    }).subscribe((data: accountInfo) => {
+      this.data = data;
+    });
   }
 
   openDialog() {
@@ -33,7 +59,7 @@ export class AccountInfoComponent implements OnInit {
 })
 
 export class updateAccountDialog {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public dialog: MatDialog) {
 
   }
 
@@ -41,6 +67,7 @@ export class updateAccountDialog {
     console.log($e)
     try {
       // this.http.post<any>(this.url, { data: 'Hello From the other side' });
+      this.dialog.closeAll();
     } catch (err) {
       console.error('Error [submitUpdatedInfo Proc]');
     }
